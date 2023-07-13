195b774eac96195109935b6536a9ebda2e5ff463
GakuruAlex <gakurualexgicimo@gmail.com>
2023-07-13 03:45:29 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
+	return 1;
+}
+
