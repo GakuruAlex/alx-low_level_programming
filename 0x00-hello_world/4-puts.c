48630d192eb45d68da3354a8ec6581cc9e5e36af
GakuruAlex <gakurualexgicimo@gmail.com>
2023-07-13 02:46:54 -0700

    Main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main(void){
-puts("\"Programming is like building a multilingual puzzle\n");
-return 0;
+/**
+ * main-Entry point
+ * Return: always 0 (Success)
+ * *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return	(0);
 }
