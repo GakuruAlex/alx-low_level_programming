a39d2efd3a7dd783d3a4f7d61a941ae14b4d03ff
GakuruAlex <gakurualexgicimo@gmail.com>
2023-07-18 05:58:39 -0700

    Using header files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -7,7 +8,20 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+         _putchar('_');
+         _putchar('p');
+         _putchar('u');
+         _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+   	 _putchar('a');
+   	 _putchar('r');
+    	 _putchar('\n');
 
-	return (0);
+    return 0;
+}
+
+int _putchar(char c)
+{
+	return putchar(c);
 }
