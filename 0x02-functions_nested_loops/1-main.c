c86399afa6c5c63b035b04fb9711440850ea529f
GakuruAlex <gakurualexgicimo@gmail.com>
2023-07-18 06:51:09 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet -Prints lowercase alphabet
+ */
+
+void print_alphabet(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
