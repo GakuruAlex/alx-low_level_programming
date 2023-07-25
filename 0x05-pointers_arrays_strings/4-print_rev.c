6f0285b860ad78c7c144d85299128c03fa8a705b
GakuruAlex <gakurualexgicimo@gmail.com>
2023-07-25 02:15:27 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+/**
+ * print_rev - Prints a string in reverse
+ * @s: - String to print
+ */
+void print_rev(char *s)
+{
+	int j;
+
+	{
+		for (j = strlen(s); j >= 0; j--)
+		{
+		if (s[j] != '\0')
+			printf("%c", s[j]);
+		}
+	}
+}
