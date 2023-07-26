69d586a69866596330639f61e748629ebb54b6c3
GakuruAlex <gakurualexgicimo@gmail.com>
2023-07-26 04:50:37 -0700

    Capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @str: - String to capitalize
+ *
+ * Return: Always a string
+ */
+char *cap_string(char *str)
+{
+	int capitakize_next = 1;
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (isspace(str[i]) || ispunct(str[i]))
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
+			if (capitalize_next)
+			{
+				str[i] = toupper(str[i]);
+				capitalize_next = 0;
+			}
+		}
+
+	}
+}
