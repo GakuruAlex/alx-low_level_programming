fdb6942f5ff4946a6ad3e45fa60ba02d69eb0596
GakuruAlex <gakurualexgicimo@gmail.com>
2023-08-03 02:49:12 -0700

    Print String in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,23 +1,5 @@
 #include "main.h"
 /**
- * get_length - gets the length of given string
- *
- * @s: - string
- *
- * Return: - Always size of string
- */
-int get_length(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
-}
-/**
  * _print_rev_recursion - prints a string in reverse
  *
  * @s: - string to reverse
@@ -25,15 +7,10 @@ int get_length(char *s)
  */
 void _print_rev_recursion(char *s)
 {
-	int length, i;
-
-	length = get_length(s);
-
-	if (length <= 0)
+	if (*s == '\0')
 	{
 		return;
 	}
-	_putchar(s[length - 1]);
 	_print_rev_recursion(s + 1);
-
+	_putchar(*s);
 }
