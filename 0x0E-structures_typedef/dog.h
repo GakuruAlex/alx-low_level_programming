4b8bba41fe4d7a8fdd5bafc7093b3dc97452974c
GakuruAlex <gakurualexgicimo@gmail.com>
2023-08-15 09:14:05 -0700

    STRUCT

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - Creates a dog
+ *
+ * @name: Dogs name
+ *
+ * @age: age of dog
+ *
+ * @owner: Owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+}
+#endif
