acbca551d6d3ed2fd04ccd448e3542e4ec65f85e
GakuruAlex <gakurualexgicimo@gmail.com>
2023-08-24 10:59:30 -0700

    Add node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,10 +19,13 @@ typedef struct list_s
  */
 #include<stddef.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 /**
  *function prototypes
  */
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
