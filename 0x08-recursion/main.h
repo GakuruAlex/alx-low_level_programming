2ded7a1eb798c020ccb1d670c709595da5ea3418
Gakuru Alex <Gakurualexgicimo@gmail.com>
2023-08-30 03:41:11 -0700

    Is prime

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,4 +11,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int find_sqrt(int num, int root);
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 #endif
