bfcdf392ef2b264b8ac73c6290983f7db477925a
Gakuru Alex <Gakurualexgicimo@gmail.com>
2023-08-30 01:59:04 -0700

    Recursion

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -8,5 +8,8 @@
 int main(void)
 {
     _print_rev_recursion("\nColton Walker");
+    _print_rev_recursion("\nFirst, solve the problem. Then, write the code");
+    _print_rev_recursion("\nTalk is cheap. Show me the code");
+    _print_rev_recursion("");
     return (0);
 }
