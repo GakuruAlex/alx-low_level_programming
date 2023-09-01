b0bec39816667c9f891169ea4ef1e73cf0a1d1dc
Gakuru Alex <Gakurualexgicimo@gmail.com>
2023-09-01 07:07:11 -0700

    Print programs name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/** 
+ * main - prints its name
+ *
+ * Return: Always (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n",argv[0]);
+	
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * C Header files 
+ */
+#include<stdio.h>
+/**
+ * function prototypes
+ */
+int main(int argc, char *argv[]);
+#endif
