f8c041efbf5fade55840765d319cb2c0d25bb668
Gakuru Alex <Gakurualexgicimo@gmail.com>
2023-09-01 07:13:36 -0700

    Updated file to betty satandards

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,10 @@
 #include "main.h"
-/** 
+/**
  * main - prints its name
+ *
+ * @argc: Number of arguments
+ *
+ * @argv: Cmd arguments
  *
  * Return: Always (0) Success
  */
@@ -8,7 +12,6 @@ int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	printf("%s\n",argv[0]);
-	
+	printf("%s\n", argv[0]);
 	return (0);
 }
